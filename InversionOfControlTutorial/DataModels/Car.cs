b8a8f7c3278377a821ef8948bfa3b25dbb58606b
Kevin Kabatra <kevin@lsretail.com>
2020-12-08 21:13:27 +0000

    Adding Player and Car data models.
    This commit contains the business logic data models for a Player and a Car. In this example when the Player is intialized a Car is also created. The current problems with this example are:
    1. These two classes are tightly coupled together as the Player includes a reference to the concrete Car class. In addition it creates a new Car and is responsible for the creation logic of the Car.
    2. If we change the name of the Car class or its constructor we will have to update the Player. This is not such a big deal in this small scale project, but overtime as more classes take a dependency on the Car this will become a very expensive change.
    3. Because Player requires a concrete Car it is not possible to test Player in complete isolation. It is currently not possible to mock the Car class.

---
+++ InversionOfControlTutorial/DataModels/Car.cs
@@ -0,0 +1,24 @@
+﻿namespace InversionOfControlTutorial.DataModels
+{
+    public class Car
+    {
+        private bool isEngineRunning = false;
+
+        /// <summary>
+        ///     Determines the state that the engine is in.
+        /// </summary>
+        /// <returns></returns>
+        public bool GetEngineState()
+        {
+            return isEngineRunning;
+        }
+
+        /// <summary>
+        ///     Starts the engine.
+        /// </summary>
+        public void StartEngine()
+        {
+            isEngineRunning = true;
+        }
+    }
+}
---
+++ InversionOfControlTutorial/DataModels/Player.cs
@@ -0,0 +1,25 @@
+﻿namespace InversionOfControlTutorial.DataModels
+{
+    public class Player
+    {
+        private Car _car;
+
+        /// <summary>
+        ///     Constructor.
+        /// </summary>
+        public Player()
+        {
+            _car = new Car();
+            _car.StartEngine();
+        }
+
+        /// <summary>
+        ///     Determines if the player's car is currently running.
+        /// </summary>
+        /// <returns></returns>
+        public bool IsPlayersCarRunning()
+        {
+            return _car.GetEngineState();
+        }
+    }
+}
