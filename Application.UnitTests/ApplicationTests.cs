cdde9d312ad61c3aca5fbc32fd73a21a58fb22e3
Kevin Kabatra <kevin@lsretail.com>
2020-12-08 23:01:07 +0000

    Added unit testing for the application

---
+++ Application.UnitTests/ApplicationTests.cs
@@ -0,0 +1,34 @@
+﻿namespace Application.UnitTests
+{
+    using Application;
+    using Xunit;
+
+    public class ApplicationTests
+    {
+        public ApplicationTests()
+        {
+            Program.Setup();
+        }
+
+        [Fact]
+        public void CanCreateCar()
+        {
+            var car = Program.GetCar();
+            Assert.NotNull(car);
+        }
+
+        [Fact]
+        public void CanCreatePlayer()
+        {
+            var player = Program.GetPlayer();
+            Assert.NotNull(player);
+        }
+
+        [Fact]
+        public void PlayersCarIsRunning()
+        {
+            var player = Program.GetPlayer();
+            Assert.True(player.IsPlayersCarRunning());
+        }
+    }
+}
--- Application/Program.cs
+++ Application/Program.cs
@@ -2,6 +2,7 @@
 {
     using Autofac;
     using BusinessLogic.Car.DataModels;
+    using BusinessLogic.Car.Factories;
     using BusinessLogic.Player.DataModels;
     using System;
 
@@ -9,6 +10,45 @@
     {
         private static IContainer Container { get; set; }
         
+        /// <summary>
+        ///     Gets the player object.
+        /// </summary>
+        /// <returns></returns>
+        public static Player GetPlayer()
+        {
+            var player = new Player(GetCar());
+            return player;
+        }
+
+        /// <summary>
+        ///     Gets the car object.
+        /// </summary>
+        /// <returns></returns>
+        public static ICar GetCar()
+        {
+            using var scope = Container.BeginLifetimeScope();
+
+            var car = scope.Resolve<ICar>();
+            return car;
+        }
+
+        /// <summary>
+        ///     Sets up the Autofac container.
+        /// </summary>
+        public static void Setup()
+        {
+            // Create the builder
+            var builder = new ContainerBuilder();
+
+            // Expose the Car through its interface
+            // Note: You can register multiple types prior to build the container.
+            builder.RegisterType<Car>().As<ICar>();
+            builder.RegisterType<CarFactory>().AsSelf();
+
+            // Build the inversion of control container
+            Container = builder.Build();
+        }
+
         /// <summary>
         ///     The application.
         /// </summary>
@@ -18,37 +58,6 @@
             Setup();
             var player = GetPlayer();
             Console.WriteLine(String.Format("Is car running: {0}", player.IsPlayersCarRunning()));
-        }
-
-        /// <summary>
-        ///     Gets the player object.
-        /// </summary>
-        /// <returns></returns>
-        private static Player GetPlayer()
-        {
-            using (var scope = Container.BeginLifetimeScope())
-            {
-                var car = scope.Resolve<ICar>();
-                var player = new Player(car);
-                
-                return player;
-            }
-        }
-
-        /// <summary>
-        ///     Sets up the Autofac container.
-        /// </summary>
-        private static void Setup()
-        {
-            // Create the builder
-            var builder = new ContainerBuilder();
-
-            // Expose the Car through its interface
-            // Note: You can register multiple types prior to build the container.
-            builder.RegisterType<Car>().As<ICar>();
-
-            // Build the inversion of control container
-            Container = builder.Build();
         }
     }
 }
